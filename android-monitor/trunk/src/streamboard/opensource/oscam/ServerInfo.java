e6cd1fae42558663027974a5a8800998666f6317
alno <alno@97bdfe8d-690b-40ca-a606-c2e9c4a5822e>
2011-01-27 20:33:55 +0000

    Add ServerInfo class which covers Version, Start, uptime and Error information if API XML Errormessage

---
+++ android-monitor/trunk/src/streamboard/opensource/oscam/ServerInfo.java
@@ -0,0 +1,79 @@
+package streamboard.opensource.oscam;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import android.util.Log;
+
+public class ServerInfo {
+	private String version;
+	private Date startdate;
+	private Integer uptime;
+	private Boolean haserror = false;
+	private String errormessage = "undefined error";
+	
+	public String getVersion(){
+		return this.version;
+	}
+	
+	public Date getStartdate(){
+		return this.startdate;
+	}
+	
+	public Integer getUptime(){
+		return this.uptime;
+	}
+	
+	public Boolean hasError(){
+		return haserror;
+	}
+	
+	public String getErrorMessage(){
+		if (haserror){
+			return errormessage;
+		} else {
+			return "no error";
+		}
+	}
+	
+	public ServerInfo(){
+		
+	}
+	
+	public ServerInfo(Document doc){
+		try {
+		SimpleDateFormat dateparser = new SimpleDateFormat("yyyy-MM-d'T'HH:mm:ssZ"); 
+		
+		Node rootnode = doc.getElementsByTagName("oscam").item(0);
+		Element rootelement = (Element) rootnode;
+		version = rootelement.getAttribute("version");
+		
+		try {
+			startdate = dateparser.parse(rootelement.getAttribute("startdate"));
+		} catch (ParseException e) {
+			startdate = new Date();
+		}
+		uptime = Integer.parseInt(rootelement.getAttribute("uptime")); 
+		
+		NodeList nl = doc.getElementsByTagName("error");
+		if (nl != null) {
+			if (nl.getLength() > 0){
+				haserror = true;
+				Node errornode = nl.item(0);
+				if (errornode.getFirstChild() != null)
+					errormessage = errornode.getFirstChild().getNodeValue();
+			}
+		}
+		} catch (Exception e) {
+			Log.i("XML Pasing Excpetion = " , version + e.getMessage());
+		}
+		
+	}
+	
+}
