5ef7e90717b3e6f0a09cb2155f240214e9d18fb5
alno <alno@97bdfe8d-690b-40ca-a606-c2e9c4a5822e>
2011-03-16 18:27:22 +0000

    try fix FC on startup - thx AngryCockroach for stacktrace

--- src/streamboard/opensource/oscam/MainApp.java
+++ src/streamboard/opensource/oscam/MainApp.java
@@ -3,7 +3,6 @@ package streamboard.opensource.oscam;
 import java.util.ArrayList;
 
 import android.app.Application;
-import android.util.Log;
 
 public class MainApp extends Application{
 
@@ -18,14 +17,8 @@ public class MainApp extends Application{
 	}
 
 	public void setClients(ArrayList<StatusClient> clients){
-		_clients = clients;
-		/*
-		_clients.clear();
-		for(int i = 0; i < clients.size(); i++){
-			_clients.add(clients.get(i));
-		}
-	*/
-		Log.i("Clients", "Changed to " + _clients.size());
+		if(clients != null)
+			_clients = clients;
 	}
 
 
